09efd6ee5d98723613007fc36fc9ff6acd96fc29
yend724 <yend9524@gmail.com>
2023-05-27 19:12:58 +0900

    Update scripts

--- scripts/new.js
+++ scripts/new.js
@@ -1,13 +1,19 @@
-const { execSync } = require("child_process");
-const s = "abcdefghijklmnopqrstuvwxyz0123456789";
-const n = 16;
-const r = Array.from({ length: n })
-  .map(() => s[Math.floor(Math.random() * s.length)])
-  .join("");
-const now = new Date();
-const year = now.getFullYear();
-const month = ("0" + (now.getMonth() + 1)).slice(-2);
-const date = ("0" + now.getDate()).slice(-2);
-const prefix = year + month + date;
+const { execSync } = require('child_process');
 
-execSync(`npx zenn new:article --slug ${prefix}-${r}`);
+const generateRandom = (length = 16) => {
+  const s = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const n = length;
+  const r = Array.from({ length: n })
+    .map(() => s[Math.floor(Math.random() * s.length)])
+    .join('');
+  return r;
+};
+const generateDate = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = ('0' + (now.getMonth() + 1)).slice(-2);
+  const date = ('0' + now.getDate()).slice(-2);
+  return year + month + date;
+};
+
+execSync(`npx zenn new:article --slug ${generateDate()}-${generateRandom()}`);
